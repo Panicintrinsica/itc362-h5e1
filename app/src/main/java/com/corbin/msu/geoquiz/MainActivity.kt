ea1fc38cc615dba76fda4b2096a52df19ea25dcd
Emrys Corbin <emrys@corbin.dev>
2024-01-27 17:46:54 -0600

    Finish snackbar version

--- app/src/main/java/com/corbin/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/corbin/msu/geoquiz/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.corbin.msu.geoquiz
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,13 +20,30 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.true_button, Toast.LENGTH_SHORT).show()
+//        trueButton.setOnClickListener { view: View ->
+//            Toast.makeText(this, R.string.true_button, Toast.LENGTH_SHORT).show()
+//        }
+//
+//        falseButton.setOnClickListener { view: View ->
+//            Toast.makeText(this, R.string.false_button, Toast.LENGTH_SHORT).show()
+//        }
+
+        trueButton.setOnClickListener { it: View ->
+            val snackbar = Snackbar
+                .make(it, "Correct", Snackbar.LENGTH_SHORT)
+                .setTextColor(Color.BLACK)
+                .setBackgroundTint(Color.GREEN)
+                .show()
         }
 
-        falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.false_button, Toast.LENGTH_SHORT).show()
+        falseButton.setOnClickListener { it: View ->
+            val snackbar = Snackbar
+                .make(it, "Incorrect", Snackbar.LENGTH_SHORT)
+                .setTextColor(Color.BLACK)
+                .setBackgroundTint(Color.RED)
+                .show()
         }
+
     }
 
 }
