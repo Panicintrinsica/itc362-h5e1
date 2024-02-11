77c56fd76c62519fff50c392f34eaa905228c4e7
Emrys Corbin <emrys@corbin.dev>
2024-02-11 15:17:02 -0600

    Finish Ex 1, 2, and 3

--- app/src/main/java/com/corbin/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/corbin/msu/geoquiz/MainActivity.kt
@@ -1,54 +1,78 @@
 package com.corbin.msu.geoquiz
 
-import android.graphics.Color
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
+import com.corbin.msu.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        val useSnackbar = true
-
-
-        if (useSnackbar){
-            trueButton.setOnClickListener { it: View ->
-                Snackbar
-                    .make(it, "Correct", Snackbar.LENGTH_SHORT)
-                    .setTextColor(Color.BLACK)
-                    .setBackgroundTint(Color.GREEN)
-                    .show()
-            }
-
-            falseButton.setOnClickListener { it: View ->
-                Snackbar
-                    .make(it, "Incorrect", Snackbar.LENGTH_SHORT)
-                    .setTextColor(Color.BLACK)
-                    .setBackgroundTint(Color.RED)
-                    .show()
-            }
-        } else {
-            trueButton.setOnClickListener { view: View ->
-                Toast.makeText(this, R.string.true_button, Toast.LENGTH_SHORT).show()
-            }
-
-            falseButton.setOnClickListener { view: View ->
-                Toast.makeText(this, R.string.false_button, Toast.LENGTH_SHORT).show()
-            }
+        binding.trueButton.setOnClickListener { it: View ->
+            checkAnswer(true)
         }
 
+        binding.falseButton.setOnClickListener { it: View ->
+            checkAnswer(false)
+        }
+
+        binding.questionTextView.setOnClickListener {
+            nextQuestion()
+        }
+
+        binding.nextButton.setOnClickListener {
+            nextQuestion()
+        }
+
+        binding.lastButton.setOnClickListener {
+            lastQuestion()
+        }
+
+        updateQuestion()
     }
 
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun nextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateQuestion()
+    }
+
+    private fun lastQuestion() {
+        currentIndex = (currentIndex - 1) % questionBank.size
+        updateQuestion()
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
 }
---
+++ app/src/main/java/com/corbin/msu/geoquiz/Question.kt
@@ -0,0 +1,7 @@
+package com.corbin.msu.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean){
+
+}
